3905f2d3e4ce1510cbea2e5e1261c1c3a055f07c
eli <eli.kristina@reebonz.com>
2021-04-30 10:29:15 +0700

    first initiate

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <header>
+      <span>Vue.js PWA</span>
+    </header>
+    <main>
+      <img src="./assets/img/logo.png" alt="Vue.js PWA" />
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+  @import './assets/css/main.css';
+</style>
